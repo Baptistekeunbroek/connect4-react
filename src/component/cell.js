2276d97fdb9516537daf7dc48c4a3b0ca4a3b64f
Baptiste Keunebroek <baptiste.keunebroek@efrei.net>
2024-11-17 17:49:38 +0100

    Add cell style

--- src/component/cell.js
+++ src/component/cell.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Cell = ({ value, onDrop }) => {
+  const cellClass =
+    value === "X" ? "cell cell-x" : value === "O" ? "cell cell-o" : "cell";
+
   return (
-    <div className="cell" onClick={onDrop}>
-      {value}
+    <div className={cellClass} onClick={onDrop}>
+      <p>{value === "X" || value === "O" ? value : ""}</p>
     </div>
   );
 };
